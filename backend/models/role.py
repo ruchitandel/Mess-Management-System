e5c1e52aa24643ad24b32e562c460415394d872e
Ruchi Tandel <131893942+ruchitandel@users.noreply.github.com>
2026-04-02 10:22:41 +0530

    Delete backend directory

--- backend/models/role.py
+++
@@ -1,8 +0,0 @@
-from sqlalchemy import Column, Integer, String
-from database import Base
-
-class Role(Base):
-    __tablename__ = "roles"
-
-    id = Column(Integer, primary_key=True)
-    name = Column(String(100), nullable=False)
